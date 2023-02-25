1873fcb0e688375524a728538e0bb743372ee525
Coundoul <coundoulabdoulaye471@gmail.com>
2023-02-25 18:53:01 +0000

    Version Final deploiement Angular && Backend

--- src/app/entities/services/release/release.service.ts
+++ src/app/entities/services/release/release.service.ts
@@ -9,23 +9,23 @@ export class ReleaseService {
   constructor(private http: HttpClient) { }
 
   postRelease(data : any){
-    return this.http.post<any>("http://localhost:9092/releaseManager", data);
+    return this.http.post<any>("http://51.77.132.116:9092/releaseManager", data);
   }
 
   getAllRelease(){
-    return this.http.get<any>("http://localhost:9092/releaseManager");
+    return this.http.get<any>("http://51.77.132.116:9092/releaseManager");
   }
 
   putRelease(data : any, id : number){
-    return this.http.put<any>("http://localhost:9092/releaseManager/"+id, data);
+    return this.http.put<any>("http://51.77.132.116:9092/releaseManager/"+id, data);
   }
 
   getRelease(id : number){
-    return this.http.get<any>("http://localhost:9092/releaseManager/"+id);
+    return this.http.get<any>("http://51.77.132.116:9092/releaseManager/"+id);
   }
 
   deleteRelease(id : number){
-    return this.http.delete<any>("http://localhost:9092/releaseManager/"+id);
+    return this.http.delete<any>("http://51.77.132.116:9092/releaseManager/"+id);
   }
 
   identifiantRelease(){
